16345941177c0c6f476c9fd296ae09254312135a
jeongminlee <luvmini511@gmail.com>
2023-09-20 21:41:17 +0900

    solve build-array-from-permutation

---
+++ src/build-array-from-permutation/main_test.go
@@ -0,0 +1,35 @@
+package build_array_from_permutation
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "[0,2,1,5,3,4] → [0,1,2,4,5,3]",
+			args: args{[]int{0, 2, 1, 5, 3, 4}},
+			want: []int{0, 1, 2, 4, 5, 3},
+		},
+		{
+			name: "[5,0,1,2,3,4] → [4,5,0,1,2,3]",
+			args: args{[]int{5, 0, 1, 2, 3, 4}},
+			want: []int{4, 5, 0, 1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildArray(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
