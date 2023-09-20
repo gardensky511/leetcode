1f48742a1e8fa8765e0fd66a42e01ebcc9f686fb
jeongminlee <luvmini511@gmail.com>
2023-09-20 21:50:08 +0900

    add second answer to build-array-from-permutation

--- src/build-array-from-permutation/main.go
+++ src/build-array-from-permutation/main.go
@@ -2,11 +2,21 @@ package build_array_from_permutation
 
 // topic 👉 https://leetcode.com/problems/build-array-from-permutation/
 
-func buildArray(nums []int) []int {
+func buildArray1(nums []int) []int {
 	var ans []int
 
 	for i := 0; i < len(nums); i++ {
 		ans = append(ans, nums[nums[i]])
+	}
+
+	return ans
+}
+
+func buildArray2(nums []int) []int {
+	ans := make([]int, len(nums))
+
+	for index, num := range nums {
+		ans[index] = nums[num]
 	}
 
 	return ans
--- src/build-array-from-permutation/main_test.go
+++ src/build-array-from-permutation/main_test.go
@@ -27,7 +27,12 @@ func Test_buildArray(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := buildArray(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := buildArray1(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildArray() = %v, want %v", got, tt.want)
+			}
+		})
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildArray2(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("buildArray() = %v, want %v", got, tt.want)
 			}
 		})
